3cf2cf931304619c4e7ef1c25467e0b2ef28d6ef
Oussama Heloulou <oussama.hel@gmail.com>
2019-08-26 11:03:22 +0100

    Days 4 and 5

---
+++ week-01/day-04-05.js
@@ -0,0 +1,5 @@
+var x = 10
+document.write('Hello, this is my first challenge and I will dot it ' + x + ' times.')
+document.write('<br>')
+document.write('<br>')
+document.write('I will do it, I will learn JavaScript.')
