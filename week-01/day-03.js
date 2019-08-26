1cc1511bce27075de0466ddbfd4c804fd13276df
Oussama Heloulou <oussama.hel@gmail.com>
2019-08-26 10:59:39 +0100

    Day 03

---
+++ week-01/day-03.js
@@ -0,0 +1,15 @@
+var x = 5
+var y = 'هذا نص'
+var booleanTrue = true
+var boolenaFalse = false
+var noValue = null
+
+document.write(x)
+document.write('<br>')
+document.write(y)
+document.write('<br>')
+document.write(booleanTrue)
+document.write('<br>')
+document.write(booleanTrue)
+document.write('<br>')
+document.write(noValue)
